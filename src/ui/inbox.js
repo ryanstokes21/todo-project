abf8468185ba37e778c73fbc7e78b7c211cd6a81
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-08-05 20:21:57 -0500

    remove DOM code and add render task function

--- src/ui/inbox.js
+++ src/ui/inbox.js
@@ -1,26 +1,7 @@
-import { taskList } from '../components/task.js';
+import { renderTasks, taskList } from '../components/task.js';
 
 export default function renderInbox() {
   const taskContainer = document.getElementById('inbox-task-container');
 
-  for (const task of taskList) {
-    const taskCard = document.createElement('div');
-    taskCard.classList.add('task-card');
-
-    const title = document.createElement('h2');
-    title.textContent = task.title;
-
-    const description = document.createElement('p');
-    description.textContent = task.description;
-
-    const dueDate = document.createElement('p');
-    dueDate.textContent = task.dueDate;
-
-    const priority = document.createElement('p');
-    priority.textContent = task.priority;
-
-    taskCard.append(title, description, dueDate, priority);
-
-    taskContainer.append(taskCard);
-  }
+  renderTasks(taskContainer, taskList);
 }
